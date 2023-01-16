15b2182366889a801fbbc97fc8d1ffda40da1680
niraj lavani <nirajlavani@nirajs-m1.lan>
2023-01-16 13:11:25 -0500

    Finished Lists class - needs team verification

--- src/script.py
+++ src/script.py
@@ -19,15 +19,40 @@ class Numerics:
         return math.floor(n * mult + 0.5) / mult
 
 
-# local Seed,rand,rint,rnd
-# Seed=937162211
-# function rint(lo,hi) return math.floor(0.5 + rand(lo,hi)) end --> 
+# Lists Class
+class Lists:
 
-# function rand(lo,hi) --> n; a float "x" lo<=x < x
-#   lo, hi = lo or 0, hi or 1
-#   Seed = (16807 * Seed) % 2147483647
-#   return lo + (hi-lo) * Seed / 2147483647 end
+    # map a function `fun`(v) over list (skip nil results) 
+    def map(self, table, fun):
+        newTable = {}
+        for k, v in table.items():
+            v, k = fun(v)
+            if k is None:
+                newTable[len(newTable) + 1] = v
+            else:
+                newTable[k] = v
+        return newTable
 
-# function rnd(n, nPlaces) --> num. return `n` rounded to `nPlaces`
-#   local mult = 10^(nPlaces or 3)
-#   return math.floor(n * mult + 0.5) / mult end
+    # map a function `fun`(k, v) over list (skip nil results)
+    def kap(self, table, fun):
+        newTable = {}
+        for k, v in table.items():
+            v, k = fun(k, v)
+            if k is None:
+                newTable[len(newTable) + 1] = v 
+            else:
+                newTable[k] = v
+        return newTable
+
+    # return t, sorted by fun (default= <)
+    def sort(self, table, fun=None):
+        table.sort(key=fun)
+        return table
+
+    # return list of table keys, sorted
+    # -- anonymous function acquires keys from table t
+    # -- kap() is called to get a new table
+    # -- sort() is called to sort new table in ascending default order
+    def keys(self, table):
+        return self.sort(self.kap(table, lambda k, _: k))
+    

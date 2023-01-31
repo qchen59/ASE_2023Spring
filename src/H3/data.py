ff9aca41b5817fc11820a3f6a2f89b584dd7e11d
niraj lavani <nirajlavani@Nirajs-M1.local>
2023-01-31 18:07:14 -0500

    started data.half() function

--- src/H3/data.py
+++ src/H3/data.py
@@ -2,6 +2,7 @@ from utils import csv
 from row import Row
 from col import Col
 from lists import Lists
+from numerics import Numerics
 
 
 
@@ -40,3 +41,30 @@ class Data:
 
             return col.rnd(val, nPlaces), col.txt
         return self.l.kap(cols or self.cols.y, fun)
+
+    def half(i, rows=None, cols=None, above=None):
+        
+        # imports from other functions
+        numerics = Numerics()
+        lists = Lists()
+
+        def project(row):
+            return {'row': row, 'dist': numerics.cosine(dist(row, A), dist(row, B), c)}
+        
+        def dist(row1, row2):
+            return i.dist(row1, row2, cols)
+    
+        rows = rows or i.rows
+        some = numerics.many(rows, the.Sample)
+        A = above or any(some)
+        B = i.around(A, some)[int((the.Far * len(rows)) // 1)].row
+        c = dist(A, B)
+        left, right = [], []
+        for n, tmp in enumerate(lists.sort(map(project, rows), key=lambda x: x['dist'])): # No idea if this is how it works
+            if n <= len(rows) // 2:
+                left.append(tmp['row'])
+                mid = tmp['row']
+            else:
+                right.append(tmp['row'])
+        
+        return left, right, A, B, mid, c

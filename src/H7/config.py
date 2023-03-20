d775c91e4bfc49570ad9d40c45471c90caa068e6
Qiuyu Chen <christinachen0811@gmail.com>
2023-03-19 21:22:46 -0400

    updates

--- src/H7/config.py
+++ src/H7/config.py
@@ -1,20 +1,20 @@
 # Global Variable List
-the = {'bootstrap': 512, 'conf': 0.05, 'cliff': 0.4, 'cohen': 0.35, "Fmt": "%6.2f", 'width': 40}
+the = {'bootstrap': 512, 'conf': 0.05, 'cliff': 0.4, 'cohen': 0.35, "Fmt": "{:6.2f}", 'width': 40}
 # the help description
 help = """
 script.py : an example script with help text and a test suite
 USAGE:   script.py  [OPTIONS] [-g ACTION]
 OPTIONS:
   -h     --help show the help = false
-  -b     --bootstrap bt = 512
-  -s     --seed sd = 1
-  -c     --conf bt = 0.05
+  -b     --bootstrap bootstrap = 512
+  -s     --seed random number seed = 1
+  -c     --conf conf = 0.05
   -cl    --cliff bt = .4
-  -f     --file bt = ../../etc/data/auto93.csv
-  -co    --cohen bt =.35
-  -fmt   --fmt bt ={:6.2f}
-  -w     --width bt =40
-  -g     --go    bt = nothing
+  -f     --file file location = ../../etc/data/auto93.csv
+  -co    --cohen cohen =.35
+  -fmt   --fmt number format ={:6.2f}
+  -w     --width width =40
+  -g     --go go = nothing
 ACTIONS:
 """
 # Test cases
